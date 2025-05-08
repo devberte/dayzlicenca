7aef0f44fe14a6c6d4f19568db9bf8380f1ba8c0
devberte <166141808+devberte@users.noreply.github.com>
2025-05-07 22:13:48 -0300

    Update validar.php

--- validar.php
+++ validar.php
@@ -1,4 +1,5 @@
 $licencas = [
     "127.0.0.1:2302" => "OnlyGateRaid",
-    "189.106.53.15:2302" => "OnlyGateRaid"
+    "189.106.53.15:2302" => "OnlyGateRaid",
+    "189.106.53.15:2302" => "ForestFortification"
 ];
